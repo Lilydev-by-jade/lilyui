3064d6ef3851ec5febe758c3f684bfeb79aab81d
Jade Nash <jade@lilydev.com>
2024-01-13 21:28:33 -0600

    Move `$$restProps.class` to the end of Button component class attribute

--- src/lib/components/base/Button.svelte
+++ src/lib/components/base/Button.svelte
@@ -9,11 +9,10 @@
 
 <button
 	class="
-		{$$restProps.class}
 		relative flex justify-center place-items-center px-3 py-2 gap-2 rounded-md shadow-sm dark:shadow-lg transition-all scale-100 active:scale-[97%]
 		bg-zinc-100 dark:bg-zinc-800 hover:bg-zinc-50 hover:dark:bg-zinc-700 text-zinc-700 dark:text-zinc-200
 		disabled:cursor-not-allowed disabled:bg-zinc-300 disabled:text-zinc-400 disabled:dark:bg-zinc-700 disabled:dark:text-zinc-400
-		disabled:scale-100
+		disabled:scale-100 {$$restProps.class}
 	"
 	on:click
 	{disabled}
